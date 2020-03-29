c2af75aac7c7cd2ffb57d3355499c6d7d3d76e1b
mr.Blue <yashinsergey125@gmail.com>
2020-03-29 15:14:30 +0300

    Added HD-005 Добавлен слушатель к bottom navigation

--- app/src/main/java/com/healthdiary/ui/MainActivity.kt
+++ app/src/main/java/com/healthdiary/ui/MainActivity.kt
@@ -7,23 +7,40 @@ import com.healthdiary.R
 import com.healthdiary.ui.calendar.CalendarFragment
 import com.healthdiary.ui.home.HomeFragment
 import com.healthdiary.ui.profile.ProfileFragment
+import kotlinx.android.synthetic.main.activity_main.*
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
 
-    val homeFragment = HomeFragment()
-    val profileFragment = ProfileFragment()
-    val calendarFragment = CalendarFragment()
+    private val homeFragment = HomeFragment()
+    private val calendarFragment = CalendarFragment()
+    private val profileFragment = ProfileFragment()
+
+    private val bottomNavMap = mapOf(
+        R.id.bottom_nav_item_home to homeFragment,
+        R.id.bottom_nav_item_calendar to calendarFragment,
+        R.id.bottom_nav_item_profile to profileFragment
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         Timber.plant(Timber.DebugTree())
+
         displayFragment(homeFragment)
+        setBottomNavigationListener()
     }
 
     private fun displayFragment(fragment: Fragment) {
         supportFragmentManager.beginTransaction().replace(R.id.container, fragment).commit()
+    }
+
+    private fun setBottomNavigationListener() {
+        bottom_nav_home.setOnNavigationItemSelectedListener { menuItem ->
+            val fragment = bottomNavMap[menuItem.itemId]
+            fragment?.let { displayFragment(it) }
+            return@setOnNavigationItemSelectedListener true
+        }
     }
 }
